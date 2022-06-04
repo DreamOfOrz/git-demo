bd6178362c299e6843e425fd0c37c66ad0b6589f
zym <1285985024@qq.com>
2022-06-04 09:58:34 +0800

    master commit2

--- src/main/java/com/bj/GitDemo.java
+++ src/main/java/com/bj/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("2222222222222");
         System.out.println("3333333333333");
         System.out.println("4444444444444");
+        System.out.println("master test");
     }
 }
