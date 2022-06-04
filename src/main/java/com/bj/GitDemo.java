9a84d21364db2ac88d20ae3939573db65f4e9b39
zym <1285985024@qq.com>
2022-06-04 09:36:31 +0800

    second commit

--- src/main/java/com/bj/GitDemo.java
+++ src/main/java/com/bj/GitDemo.java
@@ -3,5 +3,6 @@ package com.bj;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhhhhh");
+        System.out.println("2222222222222");
     }
 }
